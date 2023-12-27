b881c9771292f2be748dd3e3e20d5fe8101d89c1
TheThirdSwan <aryaswan17@gmail.com>
2023-12-27 21:05:24 +0800

    updated header, UI of inventory

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
+import logo from '../Herbalife.png';
+
 export default function Header() {
     return (
         <header>
             <h2>Aplikasi Pemeliharaan</h2>
+            <img src={logo} alt='Logo Herbalife'/>
             <h1>Nutrition Club Pegasus</h1>
         </header>
     );
--- src/pages/inventaris.js
+++ src/pages/inventaris.js
@@ -3,30 +3,47 @@ import Navigation from '../components/Nav';
 export default function Inventaris() {
     return (
         <div className="inventariscon">
-            <h3 style={{textAlign: "center"}}>Inventaris</h3>
+            <h3 style={{textAlign: "center"}}>Inventaris Nutrition Club</h3>
             <Navigation />
-            <table>
-                <tr>
-                    <th>Kode Barang</th>
-                    <th>Nama Barang</th>
-                    <th>Stok Barang</th>
-                </tr>
-                <tr>
-                    <td>01</td>
-                    <td>shake vanilla</td>
-                    <td>20</td>
-                </tr>
-                <tr>
-                    <td>02</td>
-                    <td>shake chocolate</td>
-                    <td>15</td>
-                </tr>
-                <tr>
-                    <td>03</td>
-                    <td>nrg tea</td>
-                    <td>23</td>
-                </tr>
-            </table>
+            <div className="d-flex flex-column">
+                <div>
+                    <div className="float-start p-3">
+                        <label for="kodebarang">Kode Barang</label><br/>
+                        <input type="text"name='kodebarang'/><br/>
+                        <label for="namabarang">Nama Barang</label><br/>
+                        <input type='text' name='namabarang'/><br/>
+                        <label for="stokbarang">Stok Barang</label><br/>
+                        <input type='text' name='stokbarang'/>
+                    </div>
+                    <div className="buttoninv float-end p-3">
+                        <button className="btn btn-primary text-nowrap">Isi Data</button><br/>
+                        <button className="btn btn-primary text-nowrap">Hapus Data</button><br/>
+                        <button className="btn btn-primary text-nowrap">Edit Data</button>
+                    </div>
+                </div>
+                <table>
+                    <tr>
+                        <th>Kode Barang</th>
+                        <th>Nama Barang</th>
+                        <th>Stok Barang</th>
+                    </tr>
+                    <tr>
+                        <td>01</td>
+                        <td>shake vanilla</td>
+                        <td>20</td>
+                    </tr>
+                    <tr>
+                        <td>02</td>
+                        <td>shake chocolate</td>
+                        <td>15</td>
+                    </tr>
+                    <tr>
+                        <td>03</td>
+                        <td>nrg tea</td>
+                        <td>23</td>
+                    </tr>
+                </table>
+            </div>
         </div>
     );
 }
