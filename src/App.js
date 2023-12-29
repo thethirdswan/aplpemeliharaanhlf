ac5b61f1eb61071cdbcc276cea2b2673e72575ff
TheThirdSwan <aryaswan17@gmail.com>
2023-12-29 10:54:40 +0800

    fixed nav, added quick inventory

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 function App() {
   return (
-    <div>
-      <a href="/member"><button className="member btn btn-primary">Daftar Member</button></a>
-      <a href="/inventaris"><button className="inventaris btn btn-primary">Edit Inventaris</button></a>
+    <div className="d-flex flex-column">
+      <div>
+        <a href="/member"><button className="member btn btn-primary">Daftar Member</button></a>
+        <a href="/inventaris"><button className="inventaris btn btn-primary">Edit Inventaris</button></a>
+      </div>
+      <p className="txtinv">Inventaris cepat</p>
+        <table className="quickinv">
+          <tr>
+            <th>Kode Barang</th>
+            <th>Nama Barang</th>
+            <th>Stok Barang</th>
+          </tr>
+          <tr>
+            <td>01</td>
+            <td>shake vanilla</td>
+            <td>20</td>
+          </tr>
+          <tr>
+            <td>02</td>
+            <td>shake chocolate</td>
+            <td>15</td>
+          </tr>
+          <tr>
+            <td>03</td>
+            <td>nrg tea</td>
+            <td>23</td>
+          </tr>
+        </table>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,8 @@
 export default function Navigation() {
     return (
     <div className="navigation d-none d-md-block float-start">
+        <a href="/"><button className="btn btn-primary">Home</button></a>
         <a href="/member"><button className="btn btn-primary">Member</button></a><br/>
         <a href="/inventaris"><button className="btn btn-primary">Inventaris</button></a><br/>
-        <a href="/"><button className="btn btn-primary">Home</button></a>
     </div>
 )}
