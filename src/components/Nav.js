93e678e931207ab36f67de6dfb69a6a152472807
TheThirdSwan <aryaswan17@gmail.com>
2024-01-17 19:27:35 +0800

    fixed nav, added margin on main page table

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 export default function Navigation() {
     return (
     <div className="navigation d-none d-md-block float-start">
-        <a href="/"><button className="btn btn-primary">Home</button></a>
+        <a href="/"><button className="btn btn-primary">Home</button></a><br/>
         <a href="/member"><button className="btn btn-primary">Member</button></a><br/>
         <a href="/inventaris"><button className="btn btn-primary">Inventaris</button></a><br/>
     </div>
